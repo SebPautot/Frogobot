ab2eb600d5667f54cf04cd0c0360612dee381941
TjgL <lithmoneo@gmail.com>
2026-02-13 10:34:01 +0100

    Added new emojis

--- Frogobot.Core/DiscordEmojiOptions.cs
+++ Frogobot.Core/DiscordEmojiOptions.cs
@@ -1,7 +1,23 @@
-﻿namespace Frogobot.Core;
+﻿using NetCord.Rest;
+
+namespace Frogobot.Core;
 
 public class DiscordEmojiOptions
 {
 	public const string SectionName = "DiscordEmoji";
+	
 	public string PossessPoint { get; set; } = "";
+	public string Anakin { get; set; } = "";
+	public string Slime { get; set; } = "";
+	
+	public ReactionEmojiProperties GetReactionEmojiFrom(string emojiName)
+	{
+		emojiName = emojiName.TrimStart('<');
+		emojiName = emojiName.TrimEnd('>');
+		
+		string name = emojiName.Split(':')[1];
+		ulong id = ulong.Parse(emojiName.Split(':')[2]);
+
+		return new ReactionEmojiProperties(name, id);
+	}
 }
